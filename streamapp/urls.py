a6ccb48e64c03bce16a5043902e1a308ba34b778
Sanket Sawardekar <sanketsawardekar97@gmail.com>
2020-08-03 23:43:42 +0530

    mask face url added

--- streamapp/urls.py
+++ streamapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('video_feed', views.video_feed, name='video_feed'),
     path('webcam_feed', views.webcam_feed, name='webcam_feed'),
+    path('mask_feed', views.mask_feed, name='mask_feed'),
     ]
