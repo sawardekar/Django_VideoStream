92616cd88ec960cced5fc62742ff6ff22d535875
Sanket Sawardekar <sanketsawardekar97@gmail.com>
2020-08-03 23:41:50 +0530

    mask face view added

--- streamapp/views.py
+++ streamapp/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
-from streamapp.camera import VideoCamera, IPWebCam
+from streamapp.camera import VideoCamera, IPWebCam, MaskDetect
 # Create your views here.
+
 
 def index(request):
 	return render(request, 'streamapp/home.html')
+
 
 def gen(camera):
 	while True:
 		frame = camera.get_frame()
 		yield (b'--frame\r\n'
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 def video_feed(request):
 	return StreamingHttpResponse(gen(VideoCamera()),
@@ -19,4 +22,9 @@ def video_feed(request):
 
 def webcam_feed(request):
 	return StreamingHttpResponse(gen(IPWebCam()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+
+def mask_feed(request):
+	return StreamingHttpResponse(gen(MaskDetect()),
 					content_type='multipart/x-mixed-replace; boundary=frame')
