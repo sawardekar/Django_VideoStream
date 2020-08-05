abfd42c309611c006cc0abcfe637a3ff616774ba
Sanket Sawardekar <sanketsawardekar97@gmail.com>
2020-08-05 19:44:36 +0530

    web camlive added

--- streamapp/urls.py
+++ streamapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('video_feed', views.video_feed, name='video_feed'),
     path('webcam_feed', views.webcam_feed, name='webcam_feed'),
     path('mask_feed', views.mask_feed, name='mask_feed'),
+	path('livecam_feed', views.livecam_feed, name='livecam_feed'),
     ]
